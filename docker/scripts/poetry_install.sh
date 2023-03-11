ef6adeaed7d6d46a36c9ba6205ddf592813245eb
TatsuyaOyb <ohyabu.tatsuya.15@shizuoka.ac.jp>
2023-03-11 13:29:34 +0900

    Editing to render in container

--- docker/scripts/poetry_install.sh
+++ docker/scripts/poetry_install.sh
@@ -2,4 +2,6 @@ echo 'export PATH="/etc/poetry/bin:$PATH"' >> ${HOME}/.bashrc
 source ${HOME}/.bashrc
 
 poetry config virtualenvs.create false
-poetry -vv install
+
+poetry config installer.max-workers 10
+poetry install --no-interaction --no-ansi -vv
